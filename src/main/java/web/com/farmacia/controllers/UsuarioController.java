7c808e3cd4bc114afb8f6fe3ce7e022e72200a4a
jadei <jadeiltondm@gmail.com>
2024-06-06 14:56:09 -0300

    feat: create method pegarUsuarios(é um get)

--- src/main/java/web/com/farmacia/controllers/UsuarioController.java
+++ src/main/java/web/com/farmacia/controllers/UsuarioController.java
@@ -1,5 +1,21 @@
 package web.com.farmacia.controllers;
 
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/usuario")
 public class UsuarioController {
+
+
+
+
+    private ResponseEntity<usuario> pegarUsuarios(@PathVariable Long id){
+
+
+    }
 
 }
