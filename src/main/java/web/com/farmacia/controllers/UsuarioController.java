3f8b0fe53f675960c6382a56ab8c6f0ab97d93e2
jadei <jadeiltondm@gmail.com>
2024-06-06 15:15:07 -0300

    feat: create method criarUsuarios(é um Post)

--- src/main/java/web/com/farmacia/controllers/UsuarioController.java
+++ src/main/java/web/com/farmacia/controllers/UsuarioController.java
@@ -2,10 +2,9 @@ package web.com.farmacia.controllers;
 
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.nio.file.Path;
 
 @RestController
 @RequestMapping("/usuario")
@@ -18,6 +17,12 @@ public class UsuarioController {
 
         var usuario = usuarioServidor.pegarUsuario(id);
         return ResponseEntity.ok(usuario);
+    }
+
+
+    private ResponseEntity<usuario> criarUsuarios(@RequestBody usuario criarUsuario){
+
+
     }
 
 }
