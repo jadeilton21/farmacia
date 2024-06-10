181c65c8ed261379e9a4a74a66fd3369b18409f6
jadei <jadeiltondm@gmail.com>
2024-06-10 11:02:09 -0300

    feat: create atributos Conta

--- src/main/java/web/com/farmacia/doMain/modelo/Conta.java
+++ src/main/java/web/com/farmacia/doMain/modelo/Conta.java
@@ -1,4 +1,22 @@
 package web.com.farmacia.doMain.modelo;
 
+import java.math.BigDecimal;
+
 public class Conta {
+
+
+
+    private Long id;
+
+    private String numeroConta;
+
+
+    private BigDecimal balance;
+
+    private BigDecimal limiteDaConta;
+
+
+
+
+
 }
