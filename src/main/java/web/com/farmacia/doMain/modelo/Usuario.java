6e296bb5d35d8c99b23173360c388eda6c3da11e
jadei <jadeiltondm@gmail.com>
2024-06-10 09:50:55 -0300

    feat: create atributos class Usuario

--- src/main/java/web/com/farmacia/doMain/modelo/Usuario.java
+++ src/main/java/web/com/farmacia/doMain/modelo/Usuario.java
@@ -1,4 +1,18 @@
 package web.com.farmacia.doMain.modelo;
 
 public class Usuario {
+
+
+
+    private Long id;
+    private String nome;
+    private Conta conta;
+    private Factura factura;
+    private Card card;
+    private News news;
+
+
+
+
+
 }
