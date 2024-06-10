4b8f879909e43ff8580803781a6d2ff9db0139c0
jadei <jadeiltondm@gmail.com>
2024-06-10 09:54:46 -0300

    feat: create gets

--- src/main/java/web/com/farmacia/doMain/modelo/Usuario.java
+++ src/main/java/web/com/farmacia/doMain/modelo/Usuario.java
@@ -9,10 +9,30 @@ public class Usuario {
     private Conta conta;
     private Factura factura;
     private Card card;
-    private News news;
+    private Lis<News> news;
 
 
+    public Long getId() {
+        return id;
+    }
 
+    public String getNome() {
+        return nome;
+    }
 
+    public Conta getConta() {
+        return conta;
+    }
 
+    public Factura getFactura() {
+        return factura;
+    }
+
+    public Card getCard() {
+        return card;
+    }
+
+    public Lis<News> getNews() {
+        return news;
+    }
 }
