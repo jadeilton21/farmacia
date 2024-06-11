f29b8924a20acde9ed2a6fed5826ce674b39ff07
jadei <jadeiltondm@gmail.com>
2024-06-11 15:04:23 -0300

    feat: add Entity

--- src/main/java/web/com/farmacia/doMain/modelo/Card.java
+++ src/main/java/web/com/farmacia/doMain/modelo/Card.java
@@ -1,12 +1,14 @@
 package web.com.farmacia.doMain.modelo;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 
 import javax.annotation.processing.Generated;
 import java.math.BigDecimal;
 
+@Entity(name = "tb_card")
 public class Card {
 
 
