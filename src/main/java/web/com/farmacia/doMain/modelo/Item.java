3f1dfdf9935cc610c0908d4675270c7132b9517a
jadei <jadeiltondm@gmail.com>
2024-06-12 09:59:42 -0300

    feat: add anotations

--- src/main/java/web/com/farmacia/doMain/modelo/Item.java
+++ src/main/java/web/com/farmacia/doMain/modelo/Item.java
@@ -1,12 +1,16 @@
 package web.com.farmacia.doMain.modelo;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
 
 @MappedSuperclass
 public abstract class Item {
 
 
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String icone;
