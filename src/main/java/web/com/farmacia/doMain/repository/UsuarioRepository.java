8f6243959ec16522aa1ac54ba775c344e36686ba
jadei <jadeiltondm@gmail.com>
2024-06-12 10:41:54 -0300

    feat: create repository

--- src/main/java/web/com/farmacia/doMain/repository/UsuarioRepository.java
+++ src/main/java/web/com/farmacia/doMain/repository/UsuarioRepository.java
@@ -1,8 +1,10 @@
 package web.com.farmacia.doMain.repository;
 
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import web.com.farmacia.doMain.modelo.Usuario;
 
 @Repository
-public interface UsuarioRepository {
+public interface UsuarioRepository extends JpaRepository<Usuario,Long> {
 }
