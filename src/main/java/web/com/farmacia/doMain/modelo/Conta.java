7a10e60b5495f9ac2a71a9cb4febf1f909541ee3
jadei <jadeiltondm@gmail.com>
2024-06-12 09:59:48 -0300

    feat: add anotations

--- src/main/java/web/com/farmacia/doMain/modelo/Conta.java
+++ src/main/java/web/com/farmacia/doMain/modelo/Conta.java
@@ -1,9 +1,6 @@
 package web.com.farmacia.doMain.modelo;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 import java.math.BigDecimal;
 @Entity(name = "tb_conta")
@@ -11,7 +8,7 @@ public class Conta {
 
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column(unique = true)
     private String numeroConta;
