0f6cd9fa8a587904a4015efbf846ec3617f33267
jadei <jadeiltondm@gmail.com>
2024-06-13 09:31:46 -0300

    feat: create UsuarioService

--- src/main/java/web/com/farmacia/service/UsuarioService.java
+++ src/main/java/web/com/farmacia/service/UsuarioService.java
@@ -1,4 +1,10 @@
 package web.com.farmacia.service;
 
+import web.com.farmacia.doMain.modelo.Usuario;
+
 public interface UsuarioService {
+
+    Usuario pegarUsuarios(Long id);
+
+    Usuario criarUsuarios(Usuario usuarioCriar);
 }
