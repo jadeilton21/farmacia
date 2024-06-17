ef5d92fcbc5fcaa2bf3bac5551d32d87e34adabd
jadei <jadeiltondm@gmail.com>
2024-06-17 14:05:14 -0300

    feat: implements class UsuarioService

--- src/main/java/web/com/farmacia/service/UsuarioServiceIml/UsuarioServiceImpl.java
+++ src/main/java/web/com/farmacia/service/UsuarioServiceIml/UsuarioServiceImpl.java
@@ -1,7 +1,18 @@
 package web.com.farmacia.service.UsuarioServiceIml;
 
 import org.springframework.stereotype.Service;
+import web.com.farmacia.doMain.modelo.Usuario;
+import web.com.farmacia.service.UsuarioService;
 
 @Service
-public class UsuarioServiceImpl {
+public class UsuarioServiceImpl implements UsuarioService {
+    @Override
+    public Usuario pegarUsuarios(Long id) {
+        return null;
+    }
+
+    @Override
+    public Usuario criarUsuarios(Usuario usuarioCriar) {
+        return null;
+    }
 }
