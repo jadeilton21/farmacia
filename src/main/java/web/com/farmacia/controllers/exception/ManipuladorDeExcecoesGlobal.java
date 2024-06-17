426089b5384d4a402b01790125fad8c504993714
jadei <jadeiltondm@gmail.com>
2024-06-17 11:08:42 -0300

    feat: add method handleNotFoundException

--- src/main/java/web/com/farmacia/controllers/exception/ManipuladorDeExcecoesGlobal.java
+++ src/main/java/web/com/farmacia/controllers/exception/ManipuladorDeExcecoesGlobal.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.NoSuchElementException;
 import java.util.logging.Logger;
 
 @RestControllerAdvice
@@ -21,6 +22,11 @@ public class ManipuladorDeExcecoesGlobal {
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<String> handleBusinessException(IllegalArgumentException businessException) {
         return new ResponseEntity<>(businessException.getMessage(), HttpStatus.UNPROCESSABLE_ENTITY);
+    }
+
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<String> handleNotFoundException(NoSuchElementException notFoundException) {
+        return new ResponseEntity<>("Resource ID not found.", HttpStatus.NOT_FOUND);
     }
 
 }
