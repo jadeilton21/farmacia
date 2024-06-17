05ec12ba6109eaa8b7e14ca1f87e9a4e1f90ca07
jadei <jadeiltondm@gmail.com>
2024-06-17 15:23:04 -0300

    feat: method pegarUsuarios

--- src/main/java/web/com/farmacia/service/UsuarioServiceIml/UsuarioServiceImpl.java
+++ src/main/java/web/com/farmacia/service/UsuarioServiceIml/UsuarioServiceImpl.java
@@ -1,14 +1,28 @@
 package web.com.farmacia.service.UsuarioServiceIml;
 
+import jakarta.persistence.Id;
 import org.springframework.stereotype.Service;
 import web.com.farmacia.doMain.modelo.Usuario;
+import web.com.farmacia.doMain.repository.UsuarioRepository;
 import web.com.farmacia.service.UsuarioService;
+
+import java.util.NoSuchElementException;
 
 @Service
 public class UsuarioServiceImpl implements UsuarioService {
+
+    private final UsuarioRepository usuarioRepository;
+
+    public UsuarioServiceImpl(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
+
+
     @Override
     public Usuario pegarUsuarios(Long id) {
-        return null;
+        return usuarioRepository.findById(id).orElseThrow(NoSuchElementException::new);
+
+
     }
 
     @Override
