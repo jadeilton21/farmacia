41847226c0be57e95eb5e5ff99b53077751c9878
jadei <jadeiltondm@gmail.com>
2024-06-17 10:59:13 -0300

    feat: add Logger

--- src/main/java/web/com/farmacia/controllers/exception/ManipuladorDeExcecoesGlobal.java
+++ src/main/java/web/com/farmacia/controllers/exception/ManipuladorDeExcecoesGlobal.java
@@ -1,8 +1,15 @@
 package web.com.farmacia.controllers.exception;
 
 
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.logging.Logger;
 
 @RestControllerAdvice
 public class ManipuladorDeExcecoesGlobal {
+
+
+
+    private final Logger resistrador = (Logger) LoggerFactory.getLogger(ManipuladorDeExcecoesGlobal.class);
 }
