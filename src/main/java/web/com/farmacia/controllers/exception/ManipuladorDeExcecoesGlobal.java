044f6dd8a1c52d98623886f1d51ab5b66406bf48
jadei <jadeiltondm@gmail.com>
2024-06-17 11:08:04 -0300

    feat: add method handleBusinessException

--- src/main/java/web/com/farmacia/controllers/exception/ManipuladorDeExcecoesGlobal.java
+++ src/main/java/web/com/farmacia/controllers/exception/ManipuladorDeExcecoesGlobal.java
@@ -2,6 +2,9 @@ package web.com.farmacia.controllers.exception;
 
 
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.logging.Logger;
@@ -12,4 +15,12 @@ public class ManipuladorDeExcecoesGlobal {
 
 
     private final Logger resistrador = (Logger) LoggerFactory.getLogger(ManipuladorDeExcecoesGlobal.class);
+
+
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleBusinessException(IllegalArgumentException businessException) {
+        return new ResponseEntity<>(businessException.getMessage(), HttpStatus.UNPROCESSABLE_ENTITY);
+    }
+
 }
