e9279e09b05c9045d01d1847491d3f968fc51122
jadei <jadeiltondm@gmail.com>
2024-06-20 14:08:49 -0300

    feat: add MockMvc

--- src/test/java/web/com/farmacia/controllers/exception/ManipuladorDeExcecoesGlobalTest.java
+++ src/test/java/web/com/farmacia/controllers/exception/ManipuladorDeExcecoesGlobalTest.java
@@ -1,7 +1,23 @@
 package web.com.farmacia.controllers.exception;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+
+@SpringBootTest
+@AutoConfigureMockMvc
 class ManipuladorDeExcecoesGlobalTest {
+
+
+
+    @MockBean
+    private MockMvc mockMvc;
+
 
 }
