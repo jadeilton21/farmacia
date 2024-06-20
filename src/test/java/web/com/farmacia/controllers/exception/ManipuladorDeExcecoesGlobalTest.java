010597db0f137df6725a4047952d14cc34230126
jadei <jadeiltondm@gmail.com>
2024-06-20 14:48:53 -0300

    feat: create method handleBusinessException

--- src/test/java/web/com/farmacia/controllers/exception/ManipuladorDeExcecoesGlobalTest.java
+++ src/test/java/web/com/farmacia/controllers/exception/ManipuladorDeExcecoesGlobalTest.java
@@ -6,9 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 
 @SpringBootTest
@@ -23,10 +26,15 @@ class ManipuladorDeExcecoesGlobalTest {
 
     @Test
     @DisplayName("Deve devolver código http 404 quando informações forem invalidas...")
-    void handleBusinessException() throws Exception {
 
+    void handleBusinessException_cenario1() throws Exception {
 
+        var response = mockMvc
+                .perform(post("/"))
+                .andReturn().getResponse();
 
+        assertThat(response.getStatus())
+                .isEqualTo(HttpStatus.FORBIDDEN.value());
 
     }
 }
