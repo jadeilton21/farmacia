5d8c6bf0308856141339f64a38aa038352ffb3c5
jadei <jadeiltondm@gmail.com>
2024-06-25 09:39:37 -0300

    feat: add MockMvc

--- src/test/java/web/com/farmacia/doMain/repository/UsuarioRepositoryTest.java
+++ src/test/java/web/com/farmacia/doMain/repository/UsuarioRepositoryTest.java
@@ -1,8 +1,10 @@
 package web.com.farmacia.doMain.repository;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -10,6 +12,11 @@ import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 @AutoConfigureMockMvc
 class UsuarioRepositoryTest {
+
+
+    @Autowired
+    private MockMvc mockMvc;
+
 
 
 }
