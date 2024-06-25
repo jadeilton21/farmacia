4972af3dac5f2884287d179c5e966a3ac4e72b71
jadei <jadeiltondm@gmail.com>
2024-06-25 13:52:01 -0300

    feat: add anotations

--- src/test/java/web/com/farmacia/service/UsuarioServiceIml/UsuarioServiceImplTest.java
+++ src/test/java/web/com/farmacia/service/UsuarioServiceIml/UsuarioServiceImplTest.java
@@ -1,7 +1,13 @@
 package web.com.farmacia.service.UsuarioServiceIml;
 
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+
+@SpringBootTest
+@AutoConfigureMockMvc
 class UsuarioServiceImplTest {
 
 }
