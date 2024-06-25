7c347b922b0b3fea124ed36499465253a9dacb8c
jadei <jadeiltondm@gmail.com>
2024-06-25 13:57:06 -0300

    feat: create method pegarUsuarios_cenario1

--- src/test/java/web/com/farmacia/service/UsuarioServiceIml/UsuarioServiceImplTest.java
+++ src/test/java/web/com/farmacia/service/UsuarioServiceIml/UsuarioServiceImplTest.java
@@ -1,12 +1,16 @@
 package web.com.farmacia.service.UsuarioServiceIml;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 
 @SpringBootTest
@@ -18,6 +22,17 @@ class UsuarioServiceImplTest {
 
 
     @Test
-    void pegarUsuarios_cenario1() {
+    @DisplayName("Deve Devolver Código http 404 quando informações forem invalidas")
+    void pegarUsuarios_cenario1() throws Exception {
+
+
+
+        var response = mockMvc
+                .perform(post("/")).andReturn().getResponse();
+
+
+        assertThat(response.getStatus())
+                .isEqualTo(HttpStatus.BAD_REQUEST.value());
+
     }
 }
