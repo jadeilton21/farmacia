93a16d9f01c678a1ab9dd2f21220150a7d94d7a7
jadei <jadeiltondm@gmail.com>
2024-06-25 09:10:12 -0300

    feat: create method handleUnexpectedExceptionTest.

--- src/test/java/web/com/farmacia/controllers/exception/ManipuladorDeExcecoesGlobalTest.java
+++ src/test/java/web/com/farmacia/controllers/exception/ManipuladorDeExcecoesGlobalTest.java
@@ -54,4 +54,8 @@ class ManipuladorDeExcecoesGlobalTest {
         assertThat(response.getStatus())
                 .isEqualTo(HttpStatus.BAD_REQUEST.value());
     }
+
+    @Test
+    void handleUnexpectedException() {
+    }
 }
