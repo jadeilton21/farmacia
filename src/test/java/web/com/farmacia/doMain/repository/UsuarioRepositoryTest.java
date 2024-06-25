de72593609cffef2c505dc5a25e85f64a7dcd28d
jadei <jadeiltondm@gmail.com>
2024-06-25 09:36:58 -0300

    feat: add anotations

--- src/test/java/web/com/farmacia/doMain/repository/UsuarioRepositoryTest.java
+++ src/test/java/web/com/farmacia/doMain/repository/UsuarioRepositoryTest.java
@@ -1,7 +1,15 @@
 package web.com.farmacia.doMain.repository;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+
+@SpringBootTest
+@AutoConfigureMockMvc
 class UsuarioRepositoryTest {
+
 
 }
