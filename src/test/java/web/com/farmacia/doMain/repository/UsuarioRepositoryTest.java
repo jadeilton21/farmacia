5a2bba785942a109b8caa95d5046ea096b1bbaf2
jadei <jadeiltondm@gmail.com>
2024-06-25 09:57:24 -0300

    feat: completed method existsByContaNumero_cenario1

--- src/test/java/web/com/farmacia/doMain/repository/UsuarioRepositoryTest.java
+++ src/test/java/web/com/farmacia/doMain/repository/UsuarioRepositoryTest.java
@@ -1,12 +1,17 @@
 package web.com.farmacia.doMain.repository;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledForJreRange;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 
 @SpringBootTest
@@ -18,5 +23,18 @@ class UsuarioRepositoryTest {
     private MockMvc mockMvc;
 
 
+    @Test
+    @DisplayName("Deve Devolver Código Http 404 quando informações forem invalidas..")
+    void existsByContaNumero_cenario1() throws Exception {
 
+        var response = mockMvc
+                .perform(post("/")).andReturn().getResponse();
+
+
+        assertThat(response.getStatus())
+                .isEqualTo(HttpStatus.BAD_REQUEST
+                        .value());
+
+
+    }
 }
