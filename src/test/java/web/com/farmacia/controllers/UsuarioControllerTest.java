7eede0777f079e02742dd9e91cfc48355be734c6
jadei <jadeiltondm@gmail.com>
2024-07-15 15:27:09 -0300

    feat

--- src/test/java/web/com/farmacia/controllers/UsuarioControllerTest.java
+++ src/test/java/web/com/farmacia/controllers/UsuarioControllerTest.java
@@ -1,8 +1,10 @@
 package web.com.farmacia.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.core.AutoConfigureCache;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -12,5 +14,8 @@ import static org.junit.jupiter.api.Assertions.*;
 class UsuarioControllerTest {
 
 
+    @Autowired
+    private MockMvc mockMvc;
 
+    
 }
